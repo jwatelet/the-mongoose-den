9952e39a047789db5278b0ec45a7f31e177cc864
Johan Watelet <johanwatelet@gmail.com>
2024-04-23 15:00:21 +0200

    Generate devise migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,19 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 class Post < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_23_124517) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_23_125322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_23_124517) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_23_124517) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
