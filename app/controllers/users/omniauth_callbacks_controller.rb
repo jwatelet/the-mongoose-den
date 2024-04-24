bbb2c8d95c7a632a0b5408c5ddcda7f3745c7325
Johan Watelet <johanwatelet@gmail.com>
2024-04-24 19:42:52 +0200

    Add google oauth 2 to omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,24 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.create_from_provider_data(request.env["omniauth.auth"])
+    @user = User.create_from_github(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
     else
       flash[:error] = "There was a problem signing you in through Github. Please register or try signing in later."
       redirect_to new_user_registration_url
+    end
+  end
+
+  def google_oauth2
+    @user = User.create_from_google(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"].except("extra") # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -26,12 +26,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:github]
+         :omniauthable, omniauth_providers: %i[github google_oauth2]
 
-  def self.create_from_provider_data(provider_data)
+  def self.create_from_github(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
       user.email = provider_data.info.email
       user.username = provider_data.info.nickname
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  def self.create_from_google(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+      user.email = provider_data.info.email
+      user.username = provider_data.info.name
       user.password = Devise.friendly_token[0, 20]
     end
   end
