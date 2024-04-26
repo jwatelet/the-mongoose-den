dfa7e9d7044f41c60110d51094a28db99d6fb8f3
Johan Watelet <johanwatelet@gmail.com>
2024-04-26 15:02:43 +0200

    Optimize capybara fill_in

--- spec/system/profile/update_profile_spec.rb
+++ spec/system/profile/update_profile_spec.rb
@@ -10,14 +10,13 @@ RSpec.describe "Update profile" do
   context "when inputs are valid" do
     it "updates sucessfully" do
       visit edit_user_path(user)
-      fill_in "Username", with: ""
-      fill_in "Username", with: "Killer666"
+      fill_in "Username", with: "Killer666", fill_options: { clear: :backspace }
       click_on "commit"
       expect(page).to have_content("Killer666")
     end
   end
 
-  context "when an non admin user try to edit another user" do
+  context "when an non permited user try to edit another user" do
     let(:another_user) { create(:user) }
 
     it "shows an error" do
