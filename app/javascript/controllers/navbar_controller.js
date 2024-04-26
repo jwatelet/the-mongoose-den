1200d9477acec2e7bf1c1912ef1899574a9fa657
Johan Watelet <johanwatelet@gmail.com>
2024-04-26 16:31:31 +0200

    Add burger animation to stimulus

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,9 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ["menu"];
+  static targets = ["menu", "burger"];
 
   toggle() {
       this.menuTarget.classList.toggle('is-active');
+      this.burgerTarget.classList.toggle('is-active');
   }
 }
