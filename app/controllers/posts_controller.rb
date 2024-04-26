3cc4e733a4c400cd0ada527609c7ff2acd9a4b95
Johan Watelet <johanwatelet@gmail.com>
2024-04-26 19:07:48 +0200

    order post by  created_at desc

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :action_permited?, only: %i[edit update destroy]
 
   def index
-    @posts = Post.all.includes(:author)
+    @posts = Post.all.includes(:author).order(created_at: :desc)
   end
 
   def show
