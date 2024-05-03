53afef51f51888c7855ff584f4bea2f1e63d7171
Johan Watelet <johanwatelet@gmail.com>
2024-05-03 21:57:40 +0200

    Add likes on post show

--- spec/system/likes/likes_on_posts_spec.rb
+++ spec/system/likes/likes_on_posts_spec.rb
@@ -1,31 +1,55 @@
 require "rails_helper"
 
-RSpec.describe "Likes on posts" do
+RSpec.describe "Likes on post" do
   let(:user) { create(:user) }
 
   before do
     login_as(user)
   end
 
-  context "when post is not liked by the user" do
-    it "shows the like icon" do
-      create(:post, author: user)
+  context "when you are on posts lists" do
+    context "when post is not liked by the user" do
+      it "shows the like icon" do
+        create(:post, author: user)
 
-      visit posts_path
-      click_on(class: "like")
-      expect(page).to have_css(".dislike")
+        visit posts_path
+        click_on(class: "like")
+        expect(page).to have_css(".dislike")
+      end
+    end
+
+    context "when post liked by the user" do
+      let(:post) { create(:post, author: user) }
+
+      it "shows the dislike icon" do
+        create(:like, likeable: post, liker: user)
+
+        visit posts_path
+        click_on(class: "dislike")
+        expect(page).to have_css(".like")
+      end
     end
   end
 
-  context "when post liked by the user" do
-    let(:post) { create(:post, author: user) }
+  context "when you are on particular post" do
+    let(:post) {  create(:post, author: user) }
 
-    it "shows the dislike icon" do
-      create(:like, likeable: post, liker: user)
+    context "when post is not liked by the user" do
+      it "shows the like icon" do
+        visit post_path(post)
+        click_on(class: "like")
+        expect(page).to have_css(".dislike")
+      end
+    end
 
-      visit posts_path
-      click_on(class: "dislike")
-      expect(page).to have_css(".like")
+    context "when post liked by the user" do
+      it "shows the dislike icon" do
+        create(:like, likeable: post, liker: user)
+
+        visit posts_path
+        click_on(class: "dislike")
+        expect(page).to have_css(".like")
+      end
     end
   end
 end
