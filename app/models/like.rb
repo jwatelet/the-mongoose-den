a9caab5359bb456dc689e46162ea27562f5d4b38
Johan Watelet <johanwatelet@gmail.com>
2024-05-07 18:18:11 +0200

    Add notification for post liked

--- app/models/like.rb
+++ app/models/like.rb
@@ -19,7 +19,27 @@
 #  fk_rails_...  (liker_id => users.id)
 #
 class Like < ApplicationRecord
+  after_create :notify_likeable
+
   belongs_to :likeable, polymorphic: true, counter_cache: true
   belongs_to :liker, class_name: "User"
   has_many :notifications, as: :notifiable, dependent: :destroy
+
+  private
+
+  def notify_likeable
+    case likeable_type
+    when "Post"
+      notify_post_author
+    end
+  end
+
+  def notify_post_author
+    Notification.create(
+      message: "#{liker.username} liked you post",
+      notifiable: self,
+      url: Rails.application.routes.url_helpers.post_path(likeable),
+      user: likeable.author
+    )
+  end
 end
---
+++ spec/system/notifications/like_post_notification_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "Notify post like" do
+  let(:user) { create(:user) }
+
+  before do
+    login_as(user)
+  end
+
+  context "when an user like a post" do
+    let(:other_user) { create(:user) }
+    let(:post) { create(:post, author: user) }
+
+    before do
+      create(:like, liker: other_user, likeable: post)
+    end
+
+    it "shows notification" do
+      visit user_notifications_path(user)
+
+      expect(page).to have_content("#{other_user.username} liked you post")
+    end
+
+    it "shows notification counter" do
+      visit user_notifications_path(user)
+
+      within ".notification-count" do
+        expect(page).to have_content("1")
+      end
+    end
+  end
+end
