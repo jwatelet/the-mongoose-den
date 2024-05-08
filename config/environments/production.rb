e491a601693ef9b960a23472ba5192e1e77b7135
Johan Watelet <johanwatelet@gmail.com>
2024-05-08 19:56:25 +0200

    Mailgun config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: ENV.fetch("MAILGUN_API", nil),
-    domain: "the-mongoose-den.com",
+    domain: "mg.the-mongoose-den.com",
     api_host: "api.eu.mailgun.net" # Uncomment this line for EU region domains
     # timeout: 20 # Default depends on rest-client, whose default is 60s. Added in 1.2.3.
   }
