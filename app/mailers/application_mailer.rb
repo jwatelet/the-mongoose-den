29ba90df3bd4ad86326f956c5fd5493c43a58358
Johan Watelet <johanwatelet@gmail.com>
2024-05-08 20:10:13 +0200

    Update mailing config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "notifications@the-mongoose-den.com"
   layout "mailer"
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "the-mongoose-den@example.com"
+  config.mailer_sender = "notifications@the-mongoose-den.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
